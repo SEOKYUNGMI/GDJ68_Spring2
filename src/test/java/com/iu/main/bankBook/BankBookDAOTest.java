ce87b27a5023122b1558d50a6fcdb44e6c056e48
Daeki <kimdaeki@gmail.com>
2023-07-25 09:16:44 +0900

    1

---
+++ src/test/java/com/iu/main/bankBook/BankBookDAOTest.java
@@ -0,0 +1,15 @@
+package com.iu.main.bankBook;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.iu.main.Mytest;
+
+
+public class BankBookDAOTest extends Mytest {
+	
+	
+
+
+}
